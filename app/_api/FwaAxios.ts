be723497522136e2f7e73d00d1c7a681a933f734
ivan pranata <gunawanivan1999@gmail.com>
2025-08-12 17:07:50 +0700

    add axios and cookies next

---
+++ app/_api/FwaAxios.ts
@@ -0,0 +1,33 @@
+"use client";
+import axios from "axios";
+import { getCookie } from "cookies-next";
+
+const FtthAxios = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "x-api-key": "280999!FTTH",
+    // "ngrok-skip-browser-warning": "6024",
+  },
+});
+
+FtthAxios.interceptors.request.use(async (req) => {
+  // const token = await localStorage.getItem("access_token");
+  const token = getCookie("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
+});
+
+FtthAxios.interceptors.response.use(
+  async (res) => {
+    return res;
+  },
+  async (err) => {
+    // error in global place here
+    throw err;
+  }
+);
+
+export default FtthAxios;
