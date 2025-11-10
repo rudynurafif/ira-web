2e991ac75565d09e040b55698099d066ff30d7a7
rudy.nurafif <rudy.nurafif@weave.co.id>
2025-11-10 13:33:49 +0700

    fix logout api call

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -65,7 +65,7 @@ function Footer() {
           </div>
         </div>
 
-        <div className="text-center text-[10px] mt-5">ver. 1.0611.031</div>
+        <div className="text-center text-[10px] mt-5">ver. 1.1011.032</div>
       </div>
     </div>
   );
