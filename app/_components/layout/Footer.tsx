0412d7775f062723516c201931e5004d303b5522
rudy.nurafif <rudy.nurafif@weave.co.id>
2026-02-24 11:35:58 +0700

    style: update version on footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -197,7 +197,7 @@ function Footer() {
           </div>
         </div>
 
-        <div className="text-center text-[10px] mt-5">ver. 1.2302.147</div>
+        <div className="text-center text-[10px] mt-5">ver. 1.2402.147</div>
       </div>
     </div>
   );
