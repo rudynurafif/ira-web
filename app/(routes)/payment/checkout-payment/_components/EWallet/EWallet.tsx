c5bdeb655894f8e871fe41597cb9cc527ddde678
ivan pranata <gunawanivan1999@gmail.com>
2026-04-02 18:41:52 +0700

    feat: checkout payment done. butuh testing

--- app/(routes)/payment/checkout-payment/_components/EWallet/EWallet.tsx
+++ app/(routes)/payment/checkout-payment/_components/EWallet/EWallet.tsx
@@ -33,7 +33,11 @@ const EWallet = ({ data }: { data: UnifiedPaymentData }) => {
     setIsLoadingStatus(true);
 
     try {
-      const res_status = await getPaymentStatus();
+      const customerId = sessionStorage.getItem("customer_id");
+      const params = {
+        customer_code: customerId,
+      };
+      const res_status = await getPaymentStatus(params);
       const isPaid = res_status?.data?.data;
 
       setPaymentStatus(isPaid);
--- app/(routes)/payment/checkout-payment/_components/Outlet/Outlet.tsx
+++ app/(routes)/payment/checkout-payment/_components/Outlet/Outlet.tsx
@@ -37,7 +37,11 @@ function Outlet({ data }: { data: UnifiedPaymentData }) {
     setIsLoadingStatus(true);
 
     try {
-      const res_status = await getPaymentStatus();
+      const customerId = sessionStorage.getItem("customer_id");
+      const params = {
+        customer_code: customerId,
+      };
+      const res_status = await getPaymentStatus(params);
       const isPaid = res_status?.data?.data;
 
       setPaymentStatus(isPaid);
