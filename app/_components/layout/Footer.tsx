92c8fccfcf52a518406fca527554a5f2b207f8db
rudy.nurafif <rudy.nurafif@weave.co.id>
2026-05-28 12:21:52 +0700

    style: uncomment / reimplement banner from CMS

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -228,7 +228,7 @@ function Footer() {
 
         {isMounted && (
           // VERSION
-          <div className="text-center text-[10px] mt-5">ver. 1.2505.01</div>
+          <div className="text-center text-[10px] mt-5">ver. 1.2805.01</div>
         )}
       </div>
     </div>
