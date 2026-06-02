2483ebc0f9cc89ea84b97236f2e98bf0084d35c5
rudy.nurafif <rudy.nurafif@weave.co.id>
2026-06-02 15:44:46 +0700

    fix: handle 409 register

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -228,7 +228,7 @@ function Footer() {
 
         {isMounted && (
           // VERSION
-          <div className="text-center text-[10px] mt-5">ver. 1.0206.01</div>
+          <div className="text-center text-[10px] mt-5">ver. 1.0206.02</div>
         )}
       </div>
     </div>
